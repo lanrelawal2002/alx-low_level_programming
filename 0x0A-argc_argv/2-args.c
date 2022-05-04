074b7f17fe14c29f8a003ca8a5b32041db27a91d
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-05-04 22:39:52 +0000

    2-args.c task editted

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-  * main - Prints all the args content
-  * @argc: argument count
-  * @argv: argument vector
-  *
-  * Return: Always zero
-  */
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+/*Declaring variables*/
+int count = 0;
+
+if (argc > 0)
+{
+/*WHILE - Print each arguments*/
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
+}
