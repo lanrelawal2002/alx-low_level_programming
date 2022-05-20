4fe8b5e74222d9bee6dcb13077a3d51d849f6232
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-05-20 20:30:30 +0000

    new comit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 /**
  * reverse_array - reverses the content of an array of integers
  * @a: an array of integers
- * @n: number of elements of the array
+ * @n: the number of elements of the array
  * Return: returns void
  */
 
